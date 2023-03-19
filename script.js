c28e781c78caf18ea6090309118f51e0eb3a3e7f
Vincent <vincent854321@gmail.com>
2023-03-18 19:54:11 -0500

    Fixed ui and added more functionality to buttons

--- script.js
+++ script.js
@@ -6,108 +6,107 @@ const solve = document.getElementById("solve");
 let nums = [];
 let num = "";
 
-let operators ="";
+let operators = "";
 
 //Functions to populate the display
-button.forEach(function(buttonElement){
-    buttonElement.addEventListener('mouseover', function(){
-        buttonElement.style.color = "red";
-    });
-    buttonElement.addEventListener('mouseout', function(){
-        buttonElement.style.color = "black";
-    });
-    buttonElement.addEventListener('click',function(event){
+button.forEach(function (buttonElement) {
+    changeDownOpacity(buttonElement, .8);
+    changeUpOpacity(buttonElement, 1);
+    buttonElement.addEventListener('click', function (event) {
         const buttonValue = event.target.textContent;
-        display.innerHTML += buttonValue;
-        num += buttonValue;
-        console.log(num);
-    });     
+        if (buttonValue === "A/C") {
+            window.location.reload()
+        } else {
+            display.innerHTML += buttonValue;
+            num += buttonValue;
+        }
+    });
 });
 
 //Functions to populate the display
-operator.forEach(function(buttonElement){
-    buttonElement.addEventListener('mouseover', function(){
-        buttonElement.style.color = "red";
-    });
-    buttonElement.addEventListener('mouseout', function(){
-        buttonElement.style.color = "black";
-    });
-    buttonElement.addEventListener("click",function(event){
+operator.forEach(function (buttonElement) {
+    changeDownOpacity(buttonElement, .8);
+    changeUpOpacity(buttonElement, 1);
+    buttonElement.addEventListener("click", function (event) {
+        if (num !== '') {
+            nums.push(parseInt(num));
+        }
+        if (nums.length === 2) {
+            handleSolveClick();
+        }
+        num = "";
         const buttonValue = event.target.textContent;
         operators = buttonValue;
         display.innerHTML += buttonValue;
-        nums.push(parseInt(num));
-        num = "";
-
-        console.log(num);
-        console.log(nums);
-
     })
 });
 
-solve.addEventListener("mouseover", function() {
-    solve.style.color = "red";
-});
-solve.addEventListener('mouseout', function(){
-    solve.style.color = "black";
-});
-solve.addEventListener("click", function(){
+function handleSolveClick() {
     nums.push(parseInt(num));
     console.log("Attemping to solve function:");
-    console.log(parseInt(nums[0]));
-    console.log(parseInt(nums[1]));
-    console.log(operators);
+    console.log("nums at 0: " + parseInt(nums[0]));
+    console.log("nums at 1: " + parseInt(nums[1]));
+    console.log("what is the operator: " + operators);
     let answer = operate(parseInt(nums[0]), parseInt(nums[1]), operators);
-    nums = [];
+    nums = [answer];
+    console.log(nums);
+    num = "";
+    console.log(nums);
     display.innerHTML = answer.toString();
-});
+}
 
 
+function changeDownOpacity(element, opacity) {
+    element.addEventListener("mousedown", function () {
+        element.style.opacity = opacity;
+    })
+};
+function changeUpOpacity(element, opacity) {
+    element.addEventListener("mouseup", function () {
+        element.style.opacity = opacity;
+    })
+};
 
 // Step 1: function for basic math
-function addNum(num1, num2){
+function addNum(num1, num2) {
     return parseInt(num1) + parseInt(num2);
 }
 
-function subractNum(num1, num2){
+function subractNum(num1, num2) {
     return parseInt(num1) - parseInt(num2);
 }
 
-function multiplyNum(num1, num2){
+function multiplyNum(num1, num2) {
     return parseInt(num1) * parseInt(num2);
-
 }
 
-function divideNum(num1, num2){
+function divideNum(num1, num2) {
     return parseInt(num1) / parseInt(num2);
 
 }
 
 //Step 2: Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-function operate(num1, num2, operator){
-    switch(operator){
+function operate(num1, num2, operator) {
+    switch (operator) {
         case "+":
             console.log('The "+" pathway');
-            return addNum(num1,num2);
+            return addNum(num1, num2);
             break;
         case "-":
             console.log("The \"-\" pathway");
-            return subractNum(num1,num2);
+            return subractNum(num1, num2);
             break;
         case "*":
             console.log("The \"*\" pathway");
-            return multiplyNum(num1,num2);
+            return multiplyNum(num1, num2);
             break;
         case "/":
             console.log("The \"/\" pathway");
-            return divideNum(num1,num2);
+            return divideNum(num1, num2);
             break;
         default:
-            alert("Did not recieve an operator!");            
-    }   
+            alert("Did not recieve an operator!");
+    }
 }
 
-
-
-
-
+solve.addEventListener("click", handleSolveClick);
